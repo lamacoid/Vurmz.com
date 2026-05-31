cbea1e3307e354d484fdfd92935919efad528a34
Zach DeMillo <zach@vurmz.com>
2026-05-30 18:17:22 -0600

    Revert "Bring in shop aesthetic redesign"
    This reverts commit ce4e9fe7a79fcd4543e2ce73c02b3458acf42101.

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -1,7 +1,6 @@
 import SiteHeader from '@/components/SiteHeader'
 import SiteFooter from '@/components/SiteFooter'
 import CartDrawer from '@/components/shop/CartDrawer'
-import FloralFrame from '@/components/shop/aub/FloralFrame'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -11,13 +10,10 @@ export const metadata: Metadata = {
 
 export default function ShopLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-aub-beige text-aub-ink-soft min-h-screen" data-theme="shop">
+    <div className="bg-[#F0E6D3] text-[#235158] min-h-screen" data-theme="shop">
       <SiteHeader variant="shop" />
-      {/* calm beige margin around the floral-bordered content panel */}
-      <main className="min-h-screen px-3 sm:px-5 lg:px-8 pb-6 pt-3">
-        <FloralFrame pattern="strawberry-thief">
-          {children}
-        </FloralFrame>
+      <main className="min-h-screen">
+        {children}
       </main>
       <CartDrawer />
       <SiteFooter />
